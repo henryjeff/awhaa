55a638d9d5015a315b47795c12cf21319bb3badf
Henry Heffernan <henryheffernan@gmail.com>
2019-05-07 02:08:23 -0400

    Began development on AddPreppedMeal

---
+++ src/pages/AddPreppedMeal.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="ion-page">
+    <ion-header>
+      <ion-toolbar>
+        <ion-title>Add Prepared Meal</ion-title>
+      </ion-toolbar>
+    </ion-header>
+    <ion-content class="content" padding>
+      <br>
+      <ion-searchbar class="meal-search"></ion-searchbar>
+    </ion-content>
+  </div>
+</template>
+<script>
+
+export default {
+  name: "AddPreppedMeal",
+}
+
+</script>
+
+<style>
+
+.searchbar-input.sc-ion-searchbar-md{
+  border-radius: 100px!important;
+  text-align: left!important;
+}
+
+</style>
