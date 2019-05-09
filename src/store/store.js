0c42e5c73864373d976a0602e06b5eabd0de555e
Henry Heffernan <henryheffernan@gmail.com>
2019-05-09 16:26:44 -0400

    updated store methods to handle meal overview page and meal search

--- src/store/store.js
+++ src/store/store.js
@@ -19,16 +19,24 @@ export const store = new Vuex.Store({
   actions: {
     // GET USER
     async fetchUser({commit}, payload){
+      var req_url = ""
+      if(payload.user.gid != undefined){
+        req_url = `gid/${payload.user.gid}`
+      } else if (payload.user.id != undefined) {
+        req_url = `id/${payload.user.id}`
+      } else {
+        router.push('/error/ERR_INV_GET_USER_REQ')
+      }
       return new Promise((resolve, reject) => {
-        axios.get(`/api/users/${payload.user.gid}`)
+        axios.get(`/api/users/${req_url}`)
           .then((response) => {
-            if(payload.signin && payload.user.gid == response.data.gid){
+            if(payload.signin && ((payload.user.gid == response.data.gid) || (payload.user.id == response.data._id))){
               commit('SIGNIN_USER', response.data)
             }
             resolve(response)
           })
           .catch((errors) => {
-            router.push('/error/ERR_DB_POST_USER')
+            router.push('/error/ERR_DB_GET_USER')
             reject(errors)
           })
       })
@@ -42,7 +50,7 @@ export const store = new Vuex.Store({
             resolve(response)
           })
           .catch((errors) => {
-            router.push('/error/ERR_DB_GET_USER')
+            router.push('/error/ERR_DB_POST_USER')
             reject(errors)
           })
       })
@@ -51,14 +59,52 @@ export const store = new Vuex.Store({
     async postMeal({commit}, payload){
       return new Promise((resolve, reject) => {
         axios.post("/api/meals/", payload.meal)
-            .then((response) => {
-              resolve(response)
-            })
-            .catch((errors) => {
-              router.push('/error/ERR_DB_POST_MEAL')
-              reject(errors)
-            })
+          .then((response) => {
+            resolve(response)
+          })
+          .catch((errors) => {
+            router.push('/error/ERR_DB_POST_MEAL')
+            reject(errors)
+          })
       })
-    }
+    },
+    // FETCH A MEAL
+    async fetchMeal({commit}, payload){
+      return new Promise((resolve, reject) => {
+        axios.get(`/api/meals/id/${payload.id}/`)
+          .then((response) => {
+            resolve(response)
+          })
+          .catch((errors) => {
+            router.push('/error/ERR_DB_FETCH_MEAL')
+            reject(errors)
+          })
+      })
+    },
+    // SEARCH FOR MEALS
+    async fetchMeals({commit}, payload){
+      return new Promise((resolve, reject) => {
+        axios.get(`/api/meals/search?name=${payload.name}`)
+          .then((response) => {
+            resolve(response)
+          })
+          .catch((errors) => {
+            router.push('/error/ERR_DB_FETCH_MEALS')
+            reject(errors)
+          })
+      })
+    },
+    async postPreppedMeal({commit}, payload){
+      return new Promise((resolve, reject) => {
+        axios.post('/api/mymeals/', payload.preppedmeal)
+          .then((response) => {
+            resolve(response)
+          })
+          .catch((errors) => {
+            router.push('/error/ERR_DB_POST_PREPPED_MEAL')
+            reject(errors)
+          })
+      })
+    },
   }
 })
