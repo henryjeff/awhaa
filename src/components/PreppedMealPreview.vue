9fda9236a6dcb4467618c768e539480f83dcdff6
Henry Heffernan <henryheffernan@gmail.com>
2019-05-09 16:30:20 -0400

    started work on prepped meal preview

---
+++ src/components/PreppedMealPreview.vue
@@ -0,0 +1,46 @@
+<template>
+  <ion-card>
+    <div style="padding: 7px;">
+      <ion-grid>
+        <ion-row>
+          <ion-col>
+            <ion-card-subtitle style="text-align:left!important">{{meal.calories}} calories</ion-card-subtitle>
+            <ion-card-title style="text-align:left!important">{{meal.name}}</ion-card-title>
+          </ion-col>
+          <ion-col align-self-end style="text-align:right!important">
+            <ion-chip color="primary" outline="true" @click="routeToMeal()">
+              <ion-icon name="restaurant"color="primary"></ion-icon>
+              <ion-label>Prepare</ion-label>
+            </ion-chip>
+          </ion-col>
+        </ion-row>
+      </ion-grid>
+    </div>
+  </ion-card>
+</template>
+
+<script>
+export default {
+  name: "PreppedMealPreview",
+  data () {
+    return {
+      formatted_times: ""
+    }
+  },
+  props: {
+    meal: Object
+  },
+  methods: {
+    routeToMeal(){
+      this.$router.push(`/meals/overview/${this.meal._id}`)
+    }
+  }
+}
+</script>
+
+<style>
+.card{
+  border-radius: 10px;
+  text-align: left;
+}
+</style>
