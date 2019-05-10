8f4f46b9910a4b63dd1b434ddcd813f540840952
Henry Heffernan <henryheffernan@gmail.com>
2019-05-10 00:47:27 -0400

    updated prepped meals

--- src/pages/PreppedMeals.vue
+++ src/pages/PreppedMeals.vue
@@ -10,70 +10,54 @@
         <ion-title>My Prepared Meals</ion-title>
         <ion-buttons slot="primary">
           <ion-button primary @click="toRoute('/home')">
-            <ion-icon slot="icon-only" color="primary" name="information-circle-outline"></ion-icon>
+            <ion-icon slot="icon-only" color="primary" name="help-circle-outline"></ion-icon>
           </ion-button>
         </ion-buttons>
       </ion-toolbar>
     </ion-header>
     <ion-content class="content" padding>
-      <!-- <div style="text-align:left!important;">
-        <ion-title>
-          Search
-        </ion-title>
-        <ion-title>
-          <p style="font-size:14px;">Search for meals and add them to your inventory</p>
-        </ion-title>
-      </div>
-      <ion-searchbar class="meal-search chip-outline" color="light" :value="search" @input="search = $event.target.value"></ion-searchbar>
-      <ion-item class="results-label">
-        <ion-card-subtitle>{{results.length}} results of {{total_results}} dishes</ion-card-subtitle>
-      </ion-item>
-      <hr>
-      <div v-if="loading === true">
-        <ion-spinner name="crescent" color="primary"></ion-spinner>
-      </div>
-      <div v-else-if="search === '' && results === []">
-        <ion-card-subtitle>Start typing to search for dishes</ion-card-subtitle>
-      </div> -->
       <div v-if="meals.length === 0">
         <ion-card-subtitle>No meals found...</ion-card-subtitle>
       </div>
-      <div v-else-if="meals !== []" v-for="(meal, index) in meals" class="fade-in-item" v-bind:key="index">
-        <PreppedMealPreview :meal='meal' class="fade-in-item"></PreppedMealPreview>
+      <div v-else-if="meals !== []" v-for="(meal, index) in meals" v-bind:key="index">
+        <PreppedMealPreview :preppedmeal='meal'></PreppedMealPreview>
       </div>
     </ion-content>
   </div>
 </template>
 
 <script>
-// import PreppedMealPreview from '@/components/PreppedMealPreview'
+import { EventBus } from '../events';
+
+import PreppedMealPreview from '@/components/PreppedMealPreview'
 
 export default {
   name: "PreppedMeals",
   data() {
     return {
-      loading: false,
+      loading: true,
       meals: []
     }
   },
-  // components: {
-  //   // PreppedMealPreview
-  // },
-  // created() {
-  //   this.fetchAllPreppedMeals()
-  // },
+  components: {
+    PreppedMealPreview
+  },
+  created() {
+    this.fetchAllPreppedMeals()
+  },
   methods: {
     toRoute(route) {
       this.$router.push(route);
     },
-  //   fetchAllPreppedMeals() {
-  //     this.$store.dispatch("fetchPreppedMeals", {'eaten':false})
-  //       .then((response) => {
-  //         this.loading = false
-  //         this.preppedmeals = response.data
-  //         if(response.data.length == 0) this.preppedmeals = []
-  //       })
-  //   },
+    fetchAllPreppedMeals() {
+      // EventBus.$emit('start-loading', {'self':this})
+      this.$store.dispatch("fetchPreppedMeals", {'user':{'id':this.$store.state.user._id}})
+        .then((response) => {
+          this.loading = false
+          this.meals = response.data
+          if(response.data.length == 0) this.meals = []
+        })
+    },
   }
 }
 </script>
