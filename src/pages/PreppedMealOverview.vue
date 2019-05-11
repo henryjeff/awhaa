6237dc432def558e352ecfe0512eafae5f3a3f74
Henry Heffernan <henryheffernan@gmail.com>
2019-05-11 00:39:14 -0400

    updated prepped meal overview, add in  experation timer and additional routes for whether or not the meal has been eaten

--- src/pages/PreppedMealOverview.vue
+++ src/pages/PreppedMealOverview.vue
@@ -21,8 +21,9 @@
           <ion-card-title>{{preppedmeal.meal.name}}</ion-card-title>
           <ion-card-subtitle color="primary">{{preppedmeal.meal.calories}} calories</ion-card-subtitle>
         </ion-card-header>
-        <ion-card-content style="margin-top: 16px;">
-          <MealExperationTimer class="border-bottom" style="padding-bottom:10px; margin-bottom: 5px;" :preppedmeal='preppedmeal'/>
+        <ion-card-content style="margin-top: 12px;">
+          <MealExperationTimer class="border-bottom" style="padding-bottom:10px; margin-bottom: 5px;" v-if="!preppedmeal.eaten.status" :preppedmeal='preppedmeal'/>
+          <MealEatenStatus class="border-bottom" style="padding-bottom:10px; margin-bottom: 5px;" v-else :preppedmeal='preppedmeal'/>
           <MealInformation :meal='preppedmeal.meal'/>
           <MealTimes :meal='preppedmeal.meal'/>
           <MealRecipe :meal='preppedmeal.meal' :creator='creator'/>
@@ -33,9 +34,13 @@
           <ion-card-subtitle style="text-align:left!important;">Meal created by {{creator}}</ion-card-subtitle>
         </ion-card-content>
         <ion-card-content class="bottom-buttons">
-          <ion-buttons>
-            <ion-button class="eat-meal" shape="round" fill="outline" color="primary" @click="toRoute('/mymeals/')">Eat Meal</ion-button>
-            <ion-button class="delete-meal" shape="round" fill="outline" color="medium" @click="toRoute('/mymeals/')">Delete Meal</ion-button>
+          <ion-buttons v-if="!preppedmeal.eaten.status">
+            <ion-button class="eat-meal" shape="round" fill="outline" color="primary" @click="eatMeal()">Eat Meal</ion-button>
+            <ion-button class="delete-meal" shape="round" fill="outline" color="medium" @click="deleteMeal()">Delete Meal</ion-button>
+          </ion-buttons>
+          <ion-buttons v-else>
+            <ion-button class="eat-meal" shape="round" fill="outline" color="danger" @click="deleteMeal()">Delete Meal</ion-button>
+            <ion-button class="delete-meal" shape="round" fill="outline" color="medium" @click="toRoute('/mymeals/')">Back</ion-button>
           </ion-buttons>
         </ion-card-content>
       </ion-card>
@@ -47,6 +52,7 @@
 import { EventBus } from '../events';
 
 import MealExperationTimer from '@/components/MealOverview/MealExperationTimer'
+import MealEatenStatus from '@/components/MealOverview/MealEatenStatus'
 import MealInformation from '@/components/MealOverview/MealInformation'
 import MealRecipe from '@/components/MealOverview/MealRecipe'
 import MealTimes from '@/components/MealOverview/MealTimes'
@@ -63,13 +69,13 @@ export default {
   },
   components: {
     MealExperationTimer,
+    MealEatenStatus,
     MealInformation,
     MealRecipe,
     MealTimes,
   },
   mounted () {
     var id = this.$route.params.id
-    // EventBus.$emit('start-loading', {'self':this})
     this.$store.dispatch("fetchPreppedMeal", {"preppedmeal" : {"id" : id}})
       .then((response) => {
         this.preppedmeal = response.data
@@ -84,6 +90,24 @@ export default {
   methods: {
     toRoute(route){
       this.$router.push(route)
+    },
+    eatMeal(){
+      this.$router.push('/mymeals/')
+      EventBus.$emit('start-loading', {'self':this})
+      this.$store.dispatch("updatePreppedMeal", {"preppedmeal": this.preppedmeal, "update" : {"eaten" : true}})
+        .then((response) => {
+          EventBus.$emit('success-toast', {'message':'Successfully ate prepped meal'})
+          this.loading = false
+        })
+    },
+    deleteMeal(){
+      this.$router.push('/mymeals/')
+      EventBus.$emit('start-loading', {'self':this})
+      this.$store.dispatch("deletePreppedMeal", {"preppedmeal": this.preppedmeal})
+        .then((response) => {
+          EventBus.$emit('success-toast', {'message':'Successfully deleted prepped meal'})
+          this.loading = false
+        })
     },
     // removeMeal(data){
     //   EventBus.$emit('start-loading', {'self':this, 'redirect':'/meals/search'})
