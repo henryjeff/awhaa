1e83c218b486912c66dcd05f12e478a59d28a001
Henry Heffernan <henryheffernan@gmail.com>
2019-05-11 00:42:28 -0400

    added meal eaten status

---
+++ src/components/MealOverview/MealEatenStatus.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <ion-row style="margin-bottom: 5px;">
+      <ion-col align-self-start>
+        <ion-card-subtitle style="text-align:left!important">Eaten {{formatDate(preppedmeal.eaten.on)}}</ion-card-subtitle>
+      </ion-col>
+    </ion-row>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "MealEatenStatus",
+  props: {
+    preppedmeal: Object
+  },
+  methods: {
+    formatDate(date){
+      var d = date.split('-')
+      return d[1].replace(/^0+/, '') + "/" + d[2].split('T')[0].replace(/^0+/, '') + "/" + d[0]
+    }
+  }
+}
+</script>
+
+<style>
+</style>
