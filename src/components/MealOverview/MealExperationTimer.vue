4550aaf2808c3da25c497df905574d6812e4efdd
Henry Heffernan <henryheffernan@gmail.com>
2019-05-12 19:21:18 -0400

    updated meal experation progress bar to go down over time instead of up as (what was i thinking?)

--- src/components/MealOverview/MealExperationTimer.vue
+++ src/components/MealOverview/MealExperationTimer.vue
@@ -9,15 +9,15 @@
       </ion-col>
     </ion-row>
     <ion-row style="padding-left:5px;padding-right:5px;">
-      <ion-progress-bar v-if="progress_val <= 0.6" v-bind:value="progress_val" color="success"></ion-progress-bar>
-      <ion-progress-bar v-else-if="progress_val <= 0.85" v-bind:value="progress_val" color="warning"></ion-progress-bar>
-      <ion-progress-bar v-else v-bind:value="progress_val" color="danger"></ion-progress-bar>
+      <ion-progress-bar v-if="progress_val <= 0.1" v-bind:value="progress_val" color="danger"></ion-progress-bar>
+      <ion-progress-bar v-else-if="progress_val <= 0.25" v-bind:value="progress_val" color="warning"></ion-progress-bar>
+      <ion-progress-bar v-else v-bind:value="progress_val" color="success"></ion-progress-bar>
     </ion-row>
     <ion-row>
       <ion-col align-self-end>
-        <ion-card-subtitle  v-if="progress_val <= 0.6" class="meal-condition">Good Condition - {{time_left}} left</ion-card-subtitle>
-        <ion-card-subtitle  v-else-if="progress_val <= 0.85" class="meal-condition" >Eat Soon -  {{time_left}}</ion-card-subtitle>
-        <ion-card-subtitle  v-else class="meal-condition">Expires Soon {{time_left}}</ion-card-subtitle>
+        <ion-card-subtitle  v-if="progress_val <= 0.1" class="meal-condition">Expires Soon {{time_left}}</ion-card-subtitle>
+        <ion-card-subtitle  v-else-if="progress_val <= 0.25" class="meal-condition" >Eat Soon -  {{time_left}}</ion-card-subtitle>
+        <ion-card-subtitle  v-else class="meal-condition">Good Condition - {{time_left}} left</ion-card-subtitle>
       </ion-col>
     </ion-row>
   </div>
@@ -45,7 +45,7 @@ export default {
     var diff_n_b = this.getDiffMinutes(now_date, by_date)
 
     this.mins_left = diff_n_b
-    this.progress_val = 1 - (diff_n_b / diff_c_b)
+    this.progress_val = (diff_n_b / diff_c_b)
 
     var hours = (this.mins_left - (this.mins_left % 60)) / 60
     var minutes = this.mins_left % 60
