2d679bb993c409f30755f292bb1562600ce4c7a7
Henry Heffernan <henryheffernan@gmail.com>
2019-05-14 14:25:07 -0400

    updated express server with propper directory

--- server/app.js
+++ server/app.js
@@ -24,7 +24,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(serveStatic(__dirname + "/dist"));
+app.use(serveStatic(__dirname + "../dist"));
 
 var mongoose = require('mongoose');
 
