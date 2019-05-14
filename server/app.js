eb1f65cd44765c6472757a7d8dda738cb15eb99b
Henry Heffernan <henryheffernan@gmail.com>
2019-05-14 14:30:59 -0400

    minor fix

--- server/app.js
+++ server/app.js
@@ -24,7 +24,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 // app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(express.static(path.join(__dirname, "../dist"));
+app.use(express.static(path.join(__dirname, "../dist")));
 
 var mongoose = require('mongoose');
 
